19fcb1c70326637ec31cf3f4c1c7a5616ed96487
Mte <wmetehan@gmail.com>
2022-09-30 03:15:36 +0300

    Projects

--- 1-4 ~ Students Marks Sum.c
+++ 1-4 ~ Students Marks Sum.c
@@ -6,10 +6,9 @@
 //Complete the following function.
 
 int marks_summation(int* marks, int number_of_students, char gender) 
-{
-    
+{   
     int sum = 0;
-    
+
     if(gender == 'g')
     {
         for(int i = 1; i < number_of_students; i += 2)

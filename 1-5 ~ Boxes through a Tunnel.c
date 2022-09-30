ab2d14ef643767295dc1b388d3c137a5b3519820
Mte <wmetehan@gmail.com>
2022-09-30 04:44:35 +0300

    Projects

---
+++ 1-5 ~ Boxes through a Tunnel.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box
+{
+	int length;
+    int width;
+    int height;
+};
+
+typedef struct box box;
+
+int get_volume(box b) 
+{
+	int res = b.height * b.length * b.width;
+    return res;
+}
+
+int is_lower_than_max_height(box b) 
+{
+	int res = 0;
+    if (b.height < 41)
+        res = 1;
+    return res;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	box *boxes = malloc(n * sizeof(box));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
+	}
+	for (int i = 0; i < n; i++) {
+		if (is_lower_than_max_height(boxes[i])) {
+			printf("%d\n", get_volume(boxes[i]));
+		}
+	}
+	return 0;
+}
---
+++ 1-6 ~ Small Triangles, Large Triangles.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct triangle
+{
+	int a;
+	int b;
+	int c;
+};
+
+typedef struct triangle triangle;
+
+void sort_by_area(triangle* tr, int n) 
+{
+    double area[n];
+    triangle temp_tr;
+    double temp_area;
+    
+    for(int i = 0; i < n; i++)
+    {
+        double p = (tr[i].a + tr[i].b + tr[i].c) / 2.00;
+        area[i] = sqrt(p * (p - tr[i].a) * (p - tr[i].b) * (p - tr[i].c));
+    }
+            
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = i + 1 ; j < n; j++)
+        {
+            if(area[i] > area[j])
+            {
+                temp_area = area[i];
+                area[i] = area[j];
+                area[j] = temp_area;
+                    
+                temp_tr = tr[i];
+                tr[i]  = tr[j];
+                tr[j] = temp_tr;  
+            }
+        }
+    }
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	triangle *tr = malloc(n * sizeof(triangle));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+	}
+	sort_by_area(tr, n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+	}
+	return 0;
+}
